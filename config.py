b35186dfa7068dc961a8a569174791ad41d758ef
Lucia21350 <khsong2135@gmail.com>
2025-02-19 18:00:59 +0900

    api modified

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ ALLOWED_EXTENSIONS = {"jpg", "png"}
 # Celery 설정
 CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_RESULT_BACKEND = "redis://redis:6379/0"
-CALLBACK_URL = "http://192.168.0.125:8080/ai/advance-diagnosis" # 백엔드 서버: "https://eofmqrcm45aaw8s.m.pipedream.net" "http://192.168.0.125:8080/ai/advance-diagnosis"
+CALLBACK_URL = "https://eofmqrcm45aaw8s.m.pipedream.net" #"http://4.230.16.220:8081/ai/advance-diagnosis" # 테스트 서버: "https://eofmqrcm45aaw8s.m.pipedream.net" "http://192.168.0.125:8080/ai/advance-diagnosis"
